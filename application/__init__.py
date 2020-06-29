1ca7db6c3d939ff75bb84b0a9003d06c1ea5934d
the-ashu <ashutosh16215@gmail.com>
2020-06-29 19:41:37 +0530

    some changes

--- application/__init__.py
+++ application/__init__.py
@@ -9,7 +9,7 @@ app.config.from_object(Config)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'ashutosh@1998'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'hospitalknit'
 
 mysql = MySQL(app)
